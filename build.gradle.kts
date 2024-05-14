b3353a1ae957aa48e1f11e90428772c880191f0c
Nikita Marunko <aerfus@yandex.ru>
2024-05-14 23:48:15 +0900

    chore(dep): bump Gradle Intellij Plugin to version 2024.1.1
    Compatibility issue with 2024.1.1
    
    Refs: #4

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.2")
+    version.set("2024.1.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
