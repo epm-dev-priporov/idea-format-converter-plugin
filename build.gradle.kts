1ff8f98f6f38134f0f2080ce7e1f5cf658c1d4e4
Nikita Marunko <aerfus@yandex.ru>
2024-05-14 23:47:28 +0900

    chore(dep): bump kotlin.jvm to version 1.9.24
    To support Gradle Intellij Plugin 2024.1.1
    
    Refs: #4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.22"
+    id("org.jetbrains.kotlin.jvm") version "1.9.24"
     id("org.jetbrains.intellij") version "1.10.1"
 }
 
